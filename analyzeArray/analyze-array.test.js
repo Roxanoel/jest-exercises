432717bdc31f99371a17c25a4b6d18d4ea8d5973
Roxanoel <roxane.noel92@gmail.com>
2022-07-26 10:12:49 +0100

    Wrote skeletons for tests in analyzeArray.

--- analyzeArray/analyze-array.test.js
+++ analyzeArray/analyze-array.test.js
@@ -1,3 +1,32 @@
 /* PROMPT: An analyzeArray function that takes an array of numbers and returns an 
 object with the following properties: average, min, max, and length. */
 
+const analyzeArray = require('./analyze-array');
+
+test('Returns an object', () => {
+
+});
+
+test('Average is correct (with numbers)', () => {
+    
+});
+
+test('Min is correct (with numbers)', () => {
+    
+});
+
+test('Max is correct (with numbers)', () => {
+    
+});
+
+test('Length is correct (with numbers)', () => {
+    
+});
+
+test('Not using an array as arg throws an error', () => {
+    
+});
+
+test('Error gets thrown when values in array are not numbers', () => {
+    
+});
